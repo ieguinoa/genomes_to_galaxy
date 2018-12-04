9e3dd83459286be10865f07ac0b3ed90f584b592
Ignacio <you@example.com>
2018-12-04 23:54:46 +0100

    some more work

--- gff_processing.py
+++ gff_processing.py
@@ -12,12 +12,12 @@ tx_features_names=['mRNA','transcript','lincRNA','snoRNA','tRNA','snRNA','SRP_RN
 gene_features_names=['gene','tRNA_gene','ncRNA_gene','lincRNA_gene','miRNA_gene']
 
 
-def get_longest_transcripts_only():
+
+def process_gff_function(gff_file,tx2gene_out,longest_tx_gff_out):
+
+def get_longest_transcripts_only(db,out_gff_path):
     #out_gff = open(sys.argv[2], 'w')
-    db = gffutils.create_db(sys.argv[1], 'test.db',merge_strategy="create_unique", force=True)
-    
-    db = gffutils.FeatureDB('test.db', keep_order=True)
-    
+    out_gff = open(out_gff_path, 'w')
     # iterate over all features
     for gene in db.features_of_type(gene_features_names):
          gene_id=gene.attributes['ID']
@@ -38,21 +38,19 @@ def get_longest_transcripts_only():
          # it could happen that gene is used to define structures other than the classic gene-mRNA-exons. For example  gene-tRNAs
          # in this case the previous loop wont catch any longest mRNA (the gene has no mRNA childs)   
          if longest_RNA != 'None':
-    	     print(gene)  # print the feature with the modified values
+    	     out_gff.write(gene+'\n')  # print the feature with the modified values
     	     longest_RNA.attributes['gene_id']=gene_id
-    	     print(longest_RNA)
+    	     out_gff.write(longest_RNA+'\n')
     	     for exon in db.children(longest_RNA, featuretype='exon', order_by='start'):  
     			 exon.attributes['gene_id']=gene_id
-    			 print(exon)
+    			 out_gff.write(exon+'\n')
          #else:
          #    print gene_id[0] + '\tNo RNA found'
 
 
 
-def get_tx2gen_table(gff_file,filename):
+def get_tx2gene_table(db,filename):
     out_file = open(filename, 'w')
-    db = gffutils.create_db(gff_file, 'test.db', force=True, merge_strategy='create_unique')
-    db = gffutils.FeatureDB('test.db', keep_order=True)
     for gene in db.features_of_type(gene_features_names):
         for child in db.children(gene, order_by='start'):
 #           if child.featuretype=='mRNA' or child.featuretype=='rRNA' or child.featuretype=='lnc_RNA':
@@ -61,27 +59,36 @@ def get_tx2gen_table(gff_file,filename):
 
 
 
-def main():
-    parser = optparse.OptionParser()
-    #parser.add_option( '-d', '--out_dir', dest='out_dir', action='store', type="string", default=None )
-    parser.add_option( '-o', '--out_file', dest='out_file', action='store', type="string", default=None )
-    parser.add_option( '-f', '--gff_file', dest='gff_file', action='store', type="string", default=None )
-    (options, args) = parser.parse_args()
 
-    #gff_file=options.gff_file
-    #samples=options.samples_list
-
-    #create tmp file to save tx-gene table
-
-    #tmp_dir = tempfile.mkdtemp( prefix='tmp-gene-tx-table' )
-
-    #gene_tx_table = os.path.join( options.out_dir, 'tx2gene_table' )
-
-    get_tx2gen_table(options.gff_file, options.out_file)
-
-
-if __name__ == "__main__":
-    main()
+def process_gff_function(gff_file,tx2gene_out,longest_tx_gff_out):
+    db = gffutils.create_db(gff_file, 'test.db', force=True, merge_strategy='create_unique')
+    db = gffutils.FeatureDB('test.db', keep_order=True)  
+    get_tx2gene_table(db,tx2gene_out)
+    get_longest_transcripts_only(db,)
 
 
 
+#def main():
+#    parser = optparse.OptionParser()
+#    #parser.add_option( '-d', '--out_dir', dest='out_dir', action='store', type="string", default=None )
+#    parser.add_option( '-o', '--out_file', dest='out_file', action='store', type="string", default=None )
+#    parser.add_option( '-f', '--gff_file', dest='gff_file', action='store', type="string", default=None )
+#    (options, args) = parser.parse_args()
+#
+#    #gff_file=options.gff_file
+#    #samples=options.samples_list
+#
+#    #create tmp file to save tx-gene table
+#
+#    #tmp_dir = tempfile.mkdtemp( prefix='tmp-gene-tx-table' )
+#
+#    #gene_tx_table = os.path.join( options.out_dir, 'tx2gene_table' )
+#
+#    get_tx2gen_table(options.gff_file, options.out_file)
+#
+#
+#if __name__ == "__main__":
+#    main()
+#
+#
+#
--- preprocess_build_and_install.py
+++ preprocess_build_and_install.py
@@ -3,6 +3,7 @@ import sys
 from urllib.parse import urlparse
 import os
 import gff_processing
+import subprocess
 
 def uri_validator(x):
     try:
@@ -56,69 +57,79 @@ def sanitize_names(name_raw):
 
 
 
-genome_builds_file=open(sys.argv[1])
-base_build_path=sys.argv[2]
-
-
-#skip header
-genome_builds_file.readline()
-
-# Columns in the file must be (see README):
-# Genome_build_abbreviation
-# GFF description (optional)
-# Species
-# Strain / Accession / Cultivar	
-#Source	
-#Version	
-#Info URL
-#FASTA URL
-#GFF URL	
 
 
 
-## builds dictionary
-builds_gff_dict={}
-
-
-for line in genome_builds_file.readlines():
-    build_abbv,gff_desc,species,strain,source,version,info_url,fasta_uri,gff_uri=line.split('\t')
-    build_id=sanitize_name(species+'_'+strain+'_'+version)
-    if build_id not in builds_gff_dict:
-        builds_gff_dict[]
-        build_dir=os.path.join(base_path,build_id)
-        os.mkdir(build_dir)
-        out_genome_fasta=os.path.join(build_dir,build_id.genome.fa)
-        if not get_file(fasta_uri,out_genome_fasta):  
-            return 'The genome file could not be found'
-        ## ****** samtools faidx $genome_file
-        #print the genome entry in the corresponding .yaml file
+def main():
+    genome_builds_file=open(sys.argv[1])
+    base_build_path=sys.argv[2]
+    
+    
+    #skip header
+    genome_builds_file.readline()
+    
+    # Columns in the file must be (see README):
+    # Genome_build_abbreviation
+    # GFF description (optional)
+    # Species
+    # Strain / Accession / Cultivar	
+    #Source	
+    #Version	
+    #Info URL
+    #FASTA URL
+    #GFF URL	
+    
+    
+    
+    ## builds dictionary
+    builds_gff_dict={}
+    
+    
+    for line in genome_builds_file.readlines():
+        build_abbv,gff_desc,species,strain,source,version,info_url,fasta_uri,gff_uri=line.split('\t')
+        build_id=sanitize_name(species+'_'+strain+'_'+version)
+        if build_id not in builds_gff_dict:
+            builds_gff_dict[]
+            build_dir=os.path.join(base_path,build_id)
+            os.mkdir(build_dir)
+            out_genome_fasta=os.path.join(build_dir,build_id.genome.fa)
+            if not get_file(fasta_uri,out_genome_fasta):  
+                return 'The genome file could not be found'
+            ## ****** samtools faidx $genome_file
+            #print the genome entry in the corresponding .yaml file
+            
+        # Retrieve GFF
+        if (gff_desc!=''):
+            gff_id=build_id+'_'+sanitize_name(gff_desc) 
+        else:
+            gff_id=build_id+'_main_gff'
+        out_gff_path=os.path.join(build_dir,gff_id+'.gff')    
+        if not get_file(gff_uri,out_gff_path):
+            return 'The gff file could not be found'  
+    
+    
+    
+        ## Start preprocessing
+        # Get the transcriptome
+        print("------------------------")
+        print("Creating transcriptome fasta file")
+        #print("gffread -g %s -w $exome_out %s",out_genome)
+        args = [ 'gffread','-g',out_genome_fasta,'-w', transcriptome_out ]
+        proc = subprocess.Popen( args=args, shell=False)
+        return_code = proc.wait()
+        if return_code:
+            print("Error building transcriptome", file=sys.stderr)
+            sys.exit( return_code )
+    
+    
+        # Get tx2gene and representative_tx_gff
+        print("------------------------")
+        print("Processing the annotation file: extracting representativve (longest) transcript per gene and transcript to to gene table:")
+        process_gff_function(out_gff_path, tx2gene_out_path, longest_tx_out_path)
+    
         
-    # Retrieve GFF
-    if (gff_desc!=''):
-        gff_id=build_id+'_'+sanitize_name(gff_desc) 
-    else:
-        gff_id=build_id+'_main_gff'
-    out_gff_path=os.path.join(build_dir,gff_id+'.gff')    
-    if not get_file(gff_uri,out_gff_path):
-        return 'The gff file could not be found'  
-
-
-
-    ## Start preprocessing
-    # Get the transcriptome
-    print("------------------------")
-    print("Creating transcriptome fasta file with command:")
-    print("gffread -g %s -w $exome_out %s",out_genome)
-    #gffread -g $genome_file -w $exome_out $gff_full
-
-    # Get tx2gene and representative_tx_gff
-    print("------------------------")
-    print("Processing the annotation file: extracting longest transcript per gene and transcript to to gene table:")
-    gff_processing(out_gff_path, tx2gene_out_path, longest_tx_out_path)
-
-    
-    #print the gff entry and the transcriptome entry in the corresponding .yaml files
-    
+        #print the gff entry and the transcriptome entry in the corresponding .yaml files
+        
    
   
 
@@ -127,4 +138,6 @@ for line in genome_builds_file.readlines():
 
 
 
+if __name__ == "__main__":
+    main()
     
